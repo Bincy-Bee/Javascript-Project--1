ec43814c2b09495bc0c4546a30ff7990799bc26a
Bhavin Patel <patelbhavin7784@gmail.com>
2023-08-10 14:07:39 +0530

    Search-Sort method in blogs page

--- js/blogs.js
+++ js/blogs.js
@@ -1,5 +1,13 @@
-const display=(blogdata)=>{
 
+document.getElementById("menu").addEventListener("click", ()=>{
+    document.getElementById("menu-list").style.display = "block";
+});
+document.getElementById("ul-close").addEventListener("click", ()=>{
+    document.getElementById("menu-list").style.display = "none";
+});
+
+const display=(blogdata)=>{
+    document.getElementById("displayui").innerHTML="";
     blogdata.map((ele)=>{
 
         let title = document.createElement("h2");
@@ -23,6 +31,45 @@ const display=(blogdata)=>{
     })
 
 }
+
+const searchcat=()=>{
+    let serval = document.getElementById("blogsearchinput").value;
+   
+    fetch(`http://localhost:8080/addblogs?category=${serval}`)
+    .then((res)=> res.json())
+    .then((blogs)=> {
+        console.log(blogs)
+        let cat = blogs.filter((item)=> item.category.toLowerCase().match(serval.toLowerCase()));
+        display(cat);
+    })
+};
+
+document.getElementById("startsearch").addEventListener("click", searchcat);
+
+document.getElementById("blogsearchinput").addEventListener("keypress",(e)=>{
+    console.log(e.key);
+    if (e.key == "Enter"){
+        searchcat();
+    }
+});
+
+const handelcat=(cat)=>{
+    fetch(`http://localhost:8080/addblogs?category=${cat}`)
+    .then((res)=> res.json())
+    .then((blogs)=> display(blogs))
+
+}
+
+
+document.getElementById("all").addEventListener("click", ()=>get());
+document.getElementById("tech").addEventListener("click", ()=> handelcat("Technology"));
+document.getElementById("gad").addEventListener("click", ()=> handelcat("Gadgets"));
+document.getElementById("spo").addEventListener("click", ()=> handelcat("Sports"));
+document.getElementById("heal").addEventListener("click", ()=> handelcat("Health"));
+document.getElementById("edu").addEventListener("click", ()=> handelcat("Education"));
+document.getElementById("ent").addEventListener("click", ()=> handelcat("Entertainment"));
+document.getElementById("sci").addEventListener("click", ()=> handelcat("Science"));
+document.getElementById("int").addEventListener("click", ()=> handelcat("Internet"));
 
 let get = async()=>{
     fetch("http://localhost:8080/addblogs")
